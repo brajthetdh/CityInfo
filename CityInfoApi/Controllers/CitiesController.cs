0e010fec882fb690ee77b192d41c25457a80b2f1
braj <brajthetdh@gmail.com>
2020-09-06 07:30:24 +0530

    Add project files.

---
+++ CityInfoApi/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+              new List<object>() 
+            {
+                  new {id =1, Name = "ron"},
+                  new { id = 2, Name = "rton"}
+            });
+        }
+    }
+}
